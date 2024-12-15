5aa66e5c4c3bfc41c9157951b618c03a2614bd03
Githa Ary-2201010197 <117089622+ShinjiAry@users.noreply.github.com>
2024-12-16 03:15:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -342,9 +342,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         list($name, $attendance, $message_content, $local_time) = explode("|", $msg);
                         echo "
                     <div class='bg-gray-800 p-4 rounded-lg'>
-                        <h3 class='text-yellow-600 text-start'>$name</h3>
-                        <p class='text-gray-300 text-start'>$message_content</p>
-                        <p class='text-yellow-600 text-sm mt-2 text-end'>$local_time - $attendance</p>
+                        <h3 class='text-yellow-600 text-start mb-2'>$name</h3>
+                        <p class='text-gray-300 text-start text-[10px]'>$message_content</p>
+                        <p class='text-yellow-600 text-[8px] mt-2 text-end'>$local_time - $attendance</p>
                     </div>
                     ";
                     }
